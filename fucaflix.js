e21dd6256c1c61c38a1cdcd1ce1b242dd8c3c7b9
Jhone Sthan <60678458+jhonesthan@users.noreply.github.com>
2022-10-06 16:16:04 -0400

    Delete fucaflix.js

--- fucaflix.js
+++
@@ -1,9 +0,0 @@
-const nav = document.getElementById('nav');  
-
-window.addEventListener('scroll', () => {
-    if(window.scrollY >=100) {
-        nav.classList.add('nav--black')
-    } else {
-         nav.classList.remove('nav--black');
-    }
-});
